4d354aed302a3953bbb5fcf9af9b04acd810de3f
reverserob <robertarandazzo@outlook.com>
2019-08-25 21:27:36 +0200

    collection ++ custom-button

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.js';
+import Header from './components/header/header.component.js';
 
 function App() {
-  return (
-      <div>
-        <HomePage />
-      </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/shop' component={ShopPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ monsters-rolodex/build/precache-manifest.0bcdeb95265bc487d36fc8cce9f1f24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fde913c075c9246f562a9e07c1a598",
+    "url": "/complete-react-developer-zero-to-master/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d8ec47bb646888308063",
+    "url": "/complete-react-developer-zero-to-master/monster-rolodex/static/css/main.c52e20b5.chunk.css"
+  },
+  {
+    "revision": "9a15a2f8a7418d44e78e",
+    "url": "/complete-react-developer-zero-to-master/monster-rolodex/static/js/2.df0b5369.chunk.js"
+  },
+  {
+    "revision": "d8ec47bb646888308063",
+    "url": "/complete-react-developer-zero-to-master/monster-rolodex/static/js/main.ca61a4a7.chunk.js"
+  },
+  {
+    "revision": "8b35dd45b565b7887eb0",
+    "url": "/complete-react-developer-zero-to-master/monster-rolodex/static/js/runtime~main.1065a841.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/complete-react-developer-zero-to-master/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
