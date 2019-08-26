b1c257b017b722753175afaecaf3ace720b6b23b
reverserob <robertarandazzo@outlook.com>
2019-08-26 11:00:46 +0200

    firebase setup

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.js';
 import Header from './components/header/header.component.js';
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-and-sign-up.component";
+
 
 function App() {
     return (
@@ -13,7 +15,9 @@ function App() {
             <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
+                <Route exact path='/shop' component={ShopPage} />
+                <Route exact path='/signin' component={SignInAndSignUpPage} />
+
             </Switch>
         </div>
     );
