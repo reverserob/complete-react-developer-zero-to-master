b1d55bb2a24bbc50cf60d853c647591c81b2881e
reverserob <robertarandazzo@outlook.com>
2019-08-28 10:10:05 +0200

    checkoutPage

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { CheckoutPage } from './pages/checkout/checkout.component'
+import CheckoutPage from './pages/checkout/checkout.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from "./redux/user/user.selectors";
